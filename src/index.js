da26c4c6abca915d2ad086fc98b7f595c7e6adb0
mailtn <nguyetmai131203@gmail.com>
2023-09-20 10:20:01 +0700

    getListSp

--- src/index.js
+++ src/index.js
@@ -5,18 +5,21 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import "assets/plugins/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/scss/argon-dashboard-react.scss";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import AdminLayout from "layouts/Admin.js";
 import AuthLayout from "layouts/Auth.js";
+import Shop from "views/user/ListProduct.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/admin/*" element={<AdminLayout />} />
-      <Route path="/auth/*" element={<AuthLayout />} />
-      <Route path="*" element={<Navigate to="/auth/login" replace />} />
-    </Routes>
-  </BrowserRouter>
+  // <BrowserRouter>
+  //   <Routes>
+  //     <Route path="/admin/*" element={<AdminLayout />} />
+  //     <Route path="/auth/*" element={<AuthLayout />} />
+  //     <Route path="*" element={<Navigate to="/auth/login" replace />} />
+  //   </Routes>
+  // </BrowserRouter>
+  <Shop/>
 );
