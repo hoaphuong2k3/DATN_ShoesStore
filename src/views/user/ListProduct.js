112243b08619fa0c30e603b6c93418ac4489f321
mailtn <nguyetmai131203@gmail.com>
2023-09-22 15:31:07 +0700

    bill_table

--- src/views/user/ListProduct.js
+++ src/views/user/ListProduct.js
@@ -4,36 +4,36 @@ import Header from "components/Headers/ProductHeader.js";
 
 
 const Product = () => {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        fetch("https://datnshoes-default-rtdb.firebaseio.com/shoesdetails.json")
-            .then((response) => response.json())
-            .then((data) => {
-                const productsArray = Object.values(data);
-                setProducts(productsArray);
-                console.log(productsArray);
-            })
-            .catch((error) => console.log(error));
-    }, []);
+  useEffect(() => {
+    fetch("https://datnshoes-default-rtdb.firebaseio.com/shoesdetails.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const productsArray = Object.values(data);
+        setProducts(productsArray);
+        console.log(productsArray);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
 
-    return (
-        <>
-            <Header />
-            <Container fluid>
-                {/* Table */}
-                <Row>
-                    <div className="col">
-                        <Card className="shadow">
-                            <CardBody>
-                                <div style={{ display: "table", margin: "auto" }}>
-                                    <hr color="orange" width="300px" />
-                                    <br />
-                                </div>
-                                <div className="row">
-                                    <style>
-                                        {`
+  return (
+    <>
+      <Header />
+      <Container fluid>
+        {/* Table */}
+        <Row>
+          <div className="col">
+            <Card className="shadow">
+              <CardBody>
+                <div style={{ display: "table", margin: "auto" }}>
+                  <hr color="orange" width="300px" />
+                  <br />
+                </div>
+                <div className="row">
+                  <style>
+                    {`
             .zoom {
               padding: 0px;
               transition: transform .3s;
@@ -47,29 +47,29 @@ const Product = () => {
               transform: scale(1.1);
             }
           `}
-                                    </style>
-                                    {products.map((product) => (
-                                        <div key={product.code} className="col-md-3">
-                                            <a href="">
-                                                <img src={product.anh} alt="" className="zoom" />
-                                            </a>
-                                            <br />
-                                            <br />
-                                            <div style={{ fontSize: "large" }} className="p-2">
-                                                <a href="" className="text-dark text-decoration-none">{product.ten}</a>
-                                                <p className=" font-weight-bold" style={{ color: "rgba(0, 0, 0, 0.705)" }}>
-                                                    {product.gia}đ&nbsp;
-                                                </p>
-                                            </div>
-                                        </div>
-                                    ))}
-                                </div>
-                            </CardBody>
-                        </Card>
+                  </style>
+                  {products.map((product) => (
+                    <div key={product.code} className="col-md-3">
+                      <a href="">
+                        <img src={product.anh} alt="" className="zoom" />
+                      </a>
+                      <br />
+                      <br />
+                      <div style={{ fontSize: "large" }} className="p-2">
+                        <a href="" className="text-dark text-decoration-none">{product.ten}</a>
+                        <p className=" font-weight-bold" style={{ color: "rgba(0, 0, 0, 0.705)" }}>
+                          {product.gia}đ&nbsp;
+                        </p>
+                      </div>
                     </div>
-                </Row>
-            </Container>
-        </>
-    );
+                  ))}
+                </div>
+              </CardBody>
+            </Card>
+          </div>
+        </Row>
+      </Container>
+    </>
+  );
 };
 export default Product;
