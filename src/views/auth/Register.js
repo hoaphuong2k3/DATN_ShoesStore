4d2ba42775cd5e7148a37abae1efdf0895faf154
hoaphuong2k3 <hoaphuong190723@gmail.com>
2023-09-23 18:43:20 +0700

    update

--- src/views/auth/Register.js
+++ src/views/auth/Register.js
@@ -1,148 +1,154 @@
-
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import {
-    Button,
-    Card,
-    CardHeader,
-    CardBody,
-    FormGroup,
-    Form,
-    Input,
-    InputGroupAddon,
-    InputGroupText,
-    InputGroup,
-    Row,
-    Col,
-  } from "reactstrap";
-  
-  const Register = () => {
-    return (
-      <>
-        <Col lg="6" md="8">
-          <Card className="bg-secondary shadow border-0">
-            <CardHeader className="bg-transparent pb-5">
-              <div className="text-muted text-center mt-2 mb-4">
-                <small>Sign up with</small>
-              </div>
-              <div className="text-center">
-                <Button
-                  className="btn-neutral btn-icon mr-4"
-                  color="default"
-                  href="#pablo"
-                  onClick={(e) => e.preventDefault()}
-                >
-                  <span className="btn-inner--icon">
-                    <img
-                      alt="..."
-                      src={
-                        require("../../assets/img/icons/common/github.svg")
-                          .default
-                      }
-                    />
-                  </span>
-                  <span className="btn-inner--text">Github</span>
-                </Button>
-                <Button
-                  className="btn-neutral btn-icon"
-                  color="default"
-                  href="#pablo"
-                  onClick={(e) => e.preventDefault()}
-                >
-                  <span className="btn-inner--icon">
-                    <img
-                      alt="..."
-                      src={
-                        require("../../assets/img/icons/common/google.svg")
-                          .default
-                      }
-                    />
-                  </span>
-                  <span className="btn-inner--text">Google</span>
-                </Button>
-              </div>
-            </CardHeader>
-            <CardBody className="px-lg-5 py-lg-5">
-              <div className="text-center text-muted mb-4">
-                <small>Or sign up with credentials</small>
-              </div>
-              <Form role="form">
-                <FormGroup>
-                  <InputGroup className="input-group-alternative mb-3">
-                    <InputGroupAddon addonType="prepend">
-                      <InputGroupText>
-                        <i className="ni ni-hat-3" />
-                      </InputGroupText>
-                    </InputGroupAddon>
-                    <Input placeholder="Name" type="text" />
-                  </InputGroup>
-                </FormGroup>
-                <FormGroup>
-                  <InputGroup className="input-group-alternative mb-3">
-                    <InputGroupAddon addonType="prepend">
-                      <InputGroupText>
-                      <i className="ni ni-single-02" />
-                      </InputGroupText>
-                    </InputGroupAddon>
-                    <Input
-                      placeholder="Username"
-                      type="text"
-                      autoComplete="new-username"
-                    />
-                  </InputGroup>
-                </FormGroup>
-                <FormGroup>
-                  <InputGroup className="input-group-alternative">
-                    <InputGroupAddon addonType="prepend">
-                      <InputGroupText>
-                        <i className="ni ni-lock-circle-open" />
-                      </InputGroupText>
-                    </InputGroupAddon>
-                    <Input
-                      placeholder="Password"
-                      type="password"
-                      autoComplete="new-password"
-                    />
-                  </InputGroup>
-                </FormGroup>
-                <div className="text-muted font-italic">
-                  <small>
-                    password strength:{" "}
-                    <span className="text-success font-weight-700">strong</span>
-                  </small>
-                </div>
-                <Row className="my-4">
-                  <Col xs="12">
-                    <div className="custom-control custom-control-alternative custom-checkbox">
-                      <input
-                        className="custom-control-input"
-                        id="customCheckRegister"
-                        type="checkbox"
-                      />
-                      <label
-                        className="custom-control-label"
-                        htmlFor="customCheckRegister"
-                      >
-                        <span className="text-muted">
-                          I agree with the{" "}
-                          <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                            Shoes Store
-                          </a>
-                        </span>
-                      </label>
-                    </div>
-                  </Col>
-                </Row>
-                <div className="text-center">
-                  <Button className="mt-4" color="primary" type="button">
-                    Đăng ký
-                  </Button>
-                </div>
-              </Form>
-            </CardBody>
-          </Card>
-        </Col>
-      </>
-    );
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Col,
+} from "reactstrap";
+
+const Register = () => {
+  let navigate = useNavigate();
+  const [user, setUser] = useState({
+    fullname: "",
+    phoneNumber: "",
+    email: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const { fullname, phoneNumber, email, username, password, confirmPassword } = user;
+
+  const onInputChange = (e) => {
+    setUser({ ...user, [e.target.name]: [e.target.value] });
   };
-  
-  export default Register;
-  
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:33321/api/account/register", user);
+    navigate("/");
+  };
+
+
+return (
+  <>
+    <Col lg="6" md="8">
+      <Card className="bg-secondary shadow border-0">
+        <CardHeader className="bg-transparent pb-5">
+          <div className="text-muted text-center mt-2 mb-4">
+            <small>Đăng kí với</small>
+          </div>
+          <div className="text-center">
+            <Button
+              className="btn-neutral btn-icon mr-4"
+              color="default"
+              href="#"
+              onClick={(e) => e.preventDefault()}
+            >
+              <span className="btn-inner--icon">
+                <img alt="..." src={require("../../assets/img/icons/common/github.svg").default} />
+              </span>
+              <span className="btn-inner--text">Github</span>
+            </Button>
+            <Button
+              className="btn-neutral btn-icon"
+              color="default"
+              href="#"
+              onClick={(e) => e.preventDefault()}
+            >
+              <span className="btn-inner--icon">
+                <img alt="..." src={require("../../assets/img/icons/common/google.svg").default} />
+              </span>
+              <span className="btn-inner--text">Google</span>
+            </Button>
+          </div>
+        </CardHeader>
+        <CardBody className="px-lg-5 py-lg-5">
+          <div className="text-center text-muted mb-4">
+            <small>Hoặc đăng kí bằng thông tin xác thực</small>
+          </div>
+          <Form role="form"  onSubmit={(e) => onSubmit(e)}>
+            <FormGroup>
+              <InputGroup className="input-group-alternative mb-3">
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>
+                    <i className="ni ni-hat-3" />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input placeholder="Fullname" type="text" name="fullname" value={fullname} onChange={onInputChange} />
+              </InputGroup>
+            </FormGroup>
+            <FormGroup>
+              <InputGroup className="input-group-alternative mb-3">
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>
+                    <i className="ni ni-mobile-button" />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input placeholder="Phone number" type="tel" name="phoneNumber" value={phoneNumber} onChange={onInputChange} />
+              </InputGroup>
+            </FormGroup>
+            <FormGroup>
+              <InputGroup className="input-group-alternative mb-3">
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>
+                    <i className="ni ni-email-83" />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input placeholder="Email" type="email" name="email" value={email} onChange={onInputChange} />
+              </InputGroup>
+            </FormGroup>
+            <FormGroup>
+              <InputGroup className="input-group-alternative mb-3">
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>
+                    <i className="ni ni-single-02" />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input placeholder="Username" type="text" autoComplete="new-username" name="username" value={username} onChange={onInputChange} />
+              </InputGroup>
+            </FormGroup>
+            <FormGroup>
+              <InputGroup className="input-group-alternative">
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>
+                    <i className="ni ni-lock-circle-open" />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input placeholder="Password" type="password" autoComplete="new-password" name="password" value={password} onChange={onInputChange} />
+              </InputGroup>
+            </FormGroup>
+            <FormGroup>
+              <InputGroup className="input-group-alternative">
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>
+                    <i className="ni ni-key-25" />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input placeholder="Confirm Password" type="password" autoComplete="confirm-password" name="confirmPassword" value={confirmPassword} onChange={onInputChange} />
+              </InputGroup>
+            </FormGroup>
+
+            <div className="text-center">
+              <Button className="mt-4" color="primary">
+                Đăng ký
+              </Button>
+            </div>
+          </Form>
+        </CardBody>
+      </Card>
+    </Col>
+  </>
+);
+};
+
+export default Register;
