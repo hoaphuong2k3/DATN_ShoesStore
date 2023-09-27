4b077a94cead081661ebcaf8549b86ec318bd581
mailtn <nguyetmai131203@gmail.com>
2023-09-27 20:34:05 +0700

    update

--- src/views/user/Cart.js
+++ src/views/user/Cart.js
@@ -61,13 +61,13 @@ const Cart = () => {
                                                             <button type="button" class="js-qty__adjust js-qty__adjust--minus icon-fallback-text" data-id="" data-qty="1">
                                                                 <span class="icon icon-minus" aria-hidden="true"></span>
                                                                 <span class="fallback-text" aria-hidden="true">−</span>
-                                                                <span class="visually-hidden">Giảm số lượng sản phẩm đi 1</span>
+                                                                {/* <span class="visually-hidden">Giảm số lượng sản phẩm đi 1</span> */}
                                                             </button>
                                                             <input type="number" class="js-qty__num" value="2" min="1" data-id="" aria-label="quantity" pattern="[0-9]*" name="updates[]" id="updates_" />
                                                             <button type="button" class="js-qty__adjust js-qty__adjust--plus icon-fallback-text" data-id="" data-qty="21">
                                                                 <span class="icon icon-plus" aria-hidden="true"></span>
                                                                 <span class="fallback-text" aria-hidden="true">+</span>
-                                                                <span class="visually-hidden">Tăng số lượng sản phẩm lên 1</span>
+                                                                {/* <span class="visually-hidden">Tăng số lượng sản phẩm lên 1</span> */}
                                                             </button>
                                                         </div>
                                                     </td>
@@ -82,7 +82,7 @@ const Cart = () => {
                                         <div class="grid cart__row">
 
                                             <div class="grid__item two-thirds small--one-whole">
-                                                <label for="CartSpecialInstructions">Chú thích cho chủ cửa hàng</label>
+                                                <label for="CartSpecialInstructions">Chú thích cho cửa hàng</label>
                                                 <textarea name="note" class="input-full" id="CartSpecialInstructions"></textarea>
                                             </div>
 
@@ -98,97 +98,6 @@ const Cart = () => {
                                             </div>
                                         </div>
                                     </form>
-                                    <form action="/cart" method="post" novalidate="" class="cart table-wrap large--hide">
-                                        <div class="grid cart-item mg-left-0">
-                                            <div data-label="Sản phẩm" class="grid__item medium--one-third small--one-third pd-left0">
-                                                <a href="/products/giay-slip-on-double-monk-strap" class="cart__image">
-
-
-                                                    <img src="//product.hstatic.net/200000054280/product/pro-capture_one_0265_ff52ce98ad81488ba5077feb7adf69f7_medium.jpg" alt="giày da cao cấp loafer LF915" />
-
-                                                </a>
-                                            </div>
-                                            <div class="grid__item medium--two-thirds small--two-thirds pd-left15 cart-product-title">
-                                                <a href="/products/giay-slip-on-double-monk-strap" class="h4">
-                                                    giày da cao cấp loafer LF915
-                                                </a>
-
-                                                <p>Phiên bản: Nâu / 38</p>
-
-
-                                                <p>Thương hiệu: Khác</p>
-
-
-
-
-
-
-                                                Khuyến mãi:
-
-
-
-                                                1042052951 - 1499
-
-
-                                                <br />
-
-
-
-
-                                                <div data-label="Số lượng">
-
-
-                                                    <div class="js-qty">
-                                                        <button type="button" class="js-qty__adjust js-qty__adjust--minus icon-fallback-text" data-id="" data-qty="1">
-                                                            <span class="icon icon-minus" aria-hidden="true"></span>
-                                                            <span class="fallback-text" aria-hidden="true">−</span>
-                                                            <span class="visually-hidden">Giảm số lượng sản phẩm đi 1</span>
-                                                        </button>
-                                                        <input type="number" class="js-qty__num" value="2" min="1" data-id="" aria-label="quantity" pattern="[0-9]*" name="updates[]" id="updates_" />
-                                                        <button type="button" class="js-qty__adjust js-qty__adjust--plus icon-fallback-text" data-id="" data-qty="21">
-                                                            <span class="icon icon-plus" aria-hidden="true"></span>
-                                                            <span class="fallback-text" aria-hidden="true">+</span>
-                                                            <span class="visually-hidden">Tăng số lượng sản phẩm lên 1</span>
-                                                        </button>
-                                                    </div>
-
-
-                                                </div>
-                                                <div data-label="Đơn giá" class="price">
-                                                    <span class="h3">
-                                                        1,499,000₫
-                                                    </span>
-                                                </div>
-                                                <a href="/cart/change?line=1&amp;quantity=0" class="cart__remove">
-                                                    <small>Xóa</small>
-                                                </a>
-                                            </div>
-                                        </div><table class="cart-table full">
-
-
-
-                                        </table>
-                                        <div class="grid cart__row">
-
-                                            <div class="grid__item two-thirds small--one-whole">
-                                                <label for="CartSpecialInstructions">Chú thích cho chủ cửa hàng</label>
-                                                <textarea name="note" class="input-full" id="CartSpecialInstructions"></textarea>
-                                            </div>
-
-                                            <div class="grid__item text-right one-third small--one-whole">
-                                                <p>
-                                                    <span class="cart__subtotal-title">Tổng tiền</span>
-                                                    <span class="h3 cart__subtotal">2,998,000₫</span>
-                                                </p>
-
-
-                                                <button type="submit" name="update" class="btnCart update-cart">Cập nhật</button>
-                                                <button type="submit" name="checkout" class="btnCart">Thanh toán</button>
-                                            </div>
-                                        </div>
-                                    </form>
-
-
                                 </div>
                             </CardBody>
                         </Card>
