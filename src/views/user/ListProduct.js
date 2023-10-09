e15e8e1b999fa75b8c1876a4c1d298b03bd8fca2
mailtn <nguyetmai131203@gmail.com>
2023-10-10 01:40:09 +0700

    dssp_api

--- src/views/user/ListProduct.js
+++ src/views/user/ListProduct.js
@@ -2,35 +2,60 @@ import React, { useState, useEffect } from "react";
 import { Container, Row, Card, CardBody } from "reactstrap";
 import Header from "components/Headers/ProductHeader.js";
 import { Link } from 'react-router-dom';
-
+import axios from "axios";
 
 const Product = () => {
   const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState({
+    code: "",
+    name: "",
+    brandId: null,
+    originId: null,
+    designStyleId: null,
+    skinTypeId: null,
+    soleId: null,
+    liningId: null,
+    toeId: null,
+    cushionId: null,
+    fromPrice: null,
+    toPrice: null,
+    fromQuantity: null,
+    toQuantity: null,
+    fromDateStr: "",
+    toDateStr: "",
+    createdBy: ""
+  });
 
   useEffect(() => {
-    fetch("http://localhost:33321/api/user/shoes/search?page=0&size=10")
-      .then((response) => response.json())
-      .then((data) => {
-        const productsArray = Object.values(data);
-        setProducts(productsArray);
-        console.log(productsArray);
-      })
-      .catch((error) => console.log(error));
+    
+    getListShoes(0,10);
   }, []);
 
 
+  const getListShoes = async (page, size) => {
+    try {
+      const res = await axios.post(`http://localhost:33321/api/user/shoes/search?page=${page}&size=${size}`, search);
+      
+      if (res && res.data && res.data.content) {
+        setProducts(res.data.content);
+      }
+      console.log(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <>
-      <Header/>
+      <Header />
       <Container fluid>
-        {/* Table */}
         <Row>
           <div className="col">
             <Card className="shadow">
               <CardBody>
                 <div style={{ display: "table", margin: "auto" }}>
                   <hr color="orange" width="300px" />
-                  <br/>
+                  <br />
                 </div>
                 <div className="row">
                   <style>
@@ -49,23 +74,27 @@ const Product = () => {
                         }
                     `}
                   </style>
-                  {products.map((product) => (
-                    <div key={product.id} className="col-md-3">
-                      <Link to={`/shoes/productdetail/${product.id}`}>
-                        <img src={product.anh} alt="" className="zoom"/>
-                      </Link>
-                      <br/>
-                      <br/>
-                      <div style={{ fontSize: "large" }} className="p-2">
-                        <Link to={`/shoes/productdetail/${product.id}`} className="text-dark text-decoration-none">
-                          {product.name}
+                  {Array.isArray(products) ? (
+                    products.map((product) => (
+                      <div key={product.id} className="col-md-3">
+                        <Link to={`/shoes/productdetail/${product.id}`}>
+                          <img src={product.anh} alt="" className="zoom" />
                         </Link>
-                        <p className=" font-weight-bold" style={{ color: "rgba(0, 0, 0, 0.705)" }}>
-                          {product.priceMin}đ&nbsp;
-                        </p>
+                        <br />
+                        <br />
+                        <div style={{ fontSize: "large" }} className="p-2">
+                          <Link to={`/shoes/productdetail/${product.id}`} className="text-dark text-decoration-none">
+                            {product.name}
+                          </Link>
+                          <p className="font-weight-bold" style={{ color: "rgba(0, 0, 0, 0.705)" }}>
+                            {product.priceMin}đ&nbsp;
+                          </p>
+                        </div>
                       </div>
-                    </div>
-                  ))}
+                    ))
+                  ) : (
+                    <p>Không có dữ liệu.</p>
+                  )}
                 </div>
               </CardBody>
             </Card>
@@ -75,4 +104,5 @@ const Product = () => {
     </>
   );
 };
+
 export default Product;
