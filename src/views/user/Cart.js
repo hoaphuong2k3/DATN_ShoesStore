816b3c2630694f64804b68c9e2ac18cb4ecb59b6
mailtn <nguyetmai131203@gmail.com>
2023-10-13 15:20:02 +0700

    cart checkbox - quantity update

--- src/views/user/Cart.js
+++ src/views/user/Cart.js
@@ -8,6 +8,7 @@ import {
   Table,
   Input,
   Button,
+  Label,
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import Header from "components/Headers/ProductHeader";
@@ -16,6 +17,12 @@ import { CartContext } from "contexts/Cart.js";
 const Cart = () => {
   const { fetchData } = useContext(CartContext);
   const [cartData, setCartData] = useState(null);
+
+
+  const formatter = new Intl.NumberFormat('vi-VN', {
+    style: 'currency',
+    currency: 'VND',
+  });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,8 +38,6 @@ const Cart = () => {
 
     fetchData();
   }, [fetchData]);
-
-
   const handleRemoveItem = async (idAccount, idShoes) => {
     try {
       const response = await fetch(`http://localhost:33321/api/cart/delete/` + idAccount + `/` + idShoes, {
@@ -89,6 +94,7 @@ const Cart = () => {
                   {cartData && Array.isArray(cartData) && cartData.length === 0 ? (
                     <p>Giỏ hàng trống</p>
                   ) : (
+
                     <form
                       action="/cart"
                       method="post"
@@ -98,78 +104,156 @@ const Cart = () => {
                       <Table className="cart-table full table--responsive">
                         <thead className="cart__row cart__header-labels">
                           <tr>
-                            <th colSpan="2" className="text-center">
+                            {/* <th>
+                              <Input type="checkbox" className="checkbox_input" />
+                            </th> */}
+                            <th colSpan="3" className="text-center">
                               Thông tin chi tiết sản phẩm
                             </th>
                             <th className="text-center">Đơn giá</th>
                             <th className="text-center">Số lượng</th>
-                            <th className="text-right">Tổng giá</th>
+                            <th className="text-center">Tổng giá</th>
+                            <th>Thao tác</th> {/* Thêm một thẻ th trống để giữa các cột */}
                           </tr>
                         </thead>
                         <tbody>
                           {(cartData || []).map((item, index) => (
-                            <tr
-                              className="cart__row table__section"
-                              key={index}
-                            >
-                              <td data-label="Sản phẩm">
+                            <tr className="cart__row table__section" key={index}>
+
+                              <td data-label="Checkbox" className="text-center">
+                                <Input type="checkbox" className="checkbox_input" />
+                                <style>
+                                  {
+                                    `
+                              .checkbox_input {
+                                transform: scale(1.4); 
+                                margin-top: 10px;
+                              }
+                              
+                              .tongGia{
+                                color: red;
+                              }
+                              `
+                                  }
+                                </style>
+                              </td>
+                              <td data-label="Ảnh Sản phẩm">
                                 <a href="/shoes/product" className="cart__image small col-md-3">
-                                  <img src={item.anh} alt={item.name} width={'200px'} height={'200px'} />
+                                  <img
+                                    src={item.anh}
+                                    alt={item.name}
+                                    width={'200px'}
+                                    height={'200px'}
+                                  />
                                 </a>
                               </td>
-                              <td
-                                className="cart-product-title"
-                                data-label="Sản phẩm"
-                              >
-                                <a href="/shoes/product" className="h4">
-                                  {item.name}
-                                </a>
-                                <p>Size: {item.size}</p>
-                                <p>Màu: {item.color}</p>
+
+                              <td data-label="Tên Sản phẩm" className="cart-product-title">
+                                <div className="product-info">
+                                  <a href="/shoes/product" className="product-name">{item.name}</a>
+                                  <div className="product-details">
+                                    <p className="product-size">Size: {item.size}</p>
+                                    <p className="product-color">Màu: {item.color}</p>
+                                  </div>
+                                </div>
+                                <style>
+                                  {
+                                    `
+                                    .product-info {
+                                      display: flex;
+                                      align-items: center;
+                                    }
+                                    
+                                    .product-name {
+                                      font-size: 18px;
+
+                                      margin-right: 15px;
+                                    }
+                                    
+                                    .product-details {
+                                      display: flex;
+                                      flex-direction: column;
+                                    }
+                                    
+                                    .product-size,
+                                    .product-color {
+                                      font-size: 14px;
+                                      margin: 2px 0;
+                                    }
+                                    .input-group {
+                                      display: flex;
+                                      align-items: center;
+                                    }
+                                    
+                                    .quantity-input {
+                                      border: none;
+                                      outline: none;
+                                      width: 40px;
+                                      height: 30px;
+                                      padding: 0;
+                                      text-align: center;
+                                      font-size: 16px;
+                                      background-color: white;
+                                    }
+                                    
+                                    .input-group-button {
+                                      border: none;
+                                      background-color: #e9ecef;
+                                      color: black;
+                                      width: 30px;
+                                      height: 29px;
+                                      display: flex;
+                                      align-items: center;
+                                      justify-content: center;
+                                      font-size: 15px;
+                                    }   
+                                    .fa {
+                                      pointer-events: none;
+                                    }
+                              `
+                                  }
+                                </style>
                               </td>
-                              <td
-                                className="cart-product-price"
-                                data-label="Đơn giá"
-                              >
-                                <span className="h3">{item.price}</span>
+
+                              <td data-label="Đơn giá" className="text-center cart-product-price">
+                                <span className="h3">{formatter.format(item.price)}</span>
                               </td>
-                              <td
-                                data-label="Số lượng"
-                                className="cart-quantity"
-                              >
+                              <td data-label="Số lượng" className="text-center cart-quantity">
                                 <div className="input-group">
                                   <Button
-                                    className="input-group-text"
+                                    className="input-group-button"
                                     color="primary"
-                                    onClick={() =>
-                                      handleQuantityChange(1, item.id, item.quantity - 1)}
+                                    onClick={() => handleQuantityChange(-1, item.id, item.quantity - 1)}
                                   >
-                                    -
+                                    <i className="fa fa-minus" />
                                   </Button>
                                   <Input
+                                    size={1}
+                                    className="quantity-input"
                                     type="text"
-                                    className="form-control"
-                                    value={item.quantity} />
+                                    aria-valuenow={item.quantity}
+                                    value={item.quantity}
+                                    readOnly
+                                  />
                                   <Button
-                                    className="input-group-text"
+                                    className="input-group-button"
                                     color="primary"
-                                    onClick={() =>
-                                      handleQuantityChange(1, item.id, item.quantity + 1)}
+                                    onClick={() => handleQuantityChange(1, item.id, item.quantity + 1)}
                                   >
-                                    +
+                                    <i className="fa fa-plus" />
                                   </Button>
                                 </div>
                               </td>
-                              <td
-                                className="cart-product-price text-right"
-                                data-label="Tổng giá"
-                              >
-                                <span className="h3">
-                                  {item.totalPrice}
-                                </span>
+                              
+                              <td data-label="Tổng giá" className="text-center cart-product-price">
+                                <span className="h3 tongGia">{formatter.format(item.totalPrice)}</span>
                               </td>
-                              <td>
-                                <Button className="input-group-text" color="primary" onClick={() => handleRemoveItem(1, item.id)}>
+                              <td data-label="Xóa" className="text-center">
+                                <Button
+                                  className="input-group-text"
+                                  color="primary"
+                                  onClick={() => handleRemoveItem(1, item.id)}
+                                >
                                   Xóa
                                 </Button>
                               </td>
@@ -178,7 +262,6 @@ const Cart = () => {
                         </tbody>
                       </Table>
                       <div className="row">
-
                         <div className="text-right one-third small--one-whole col-md-12">
                           <Button
                             to="/shoes/checkout"
