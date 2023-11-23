577db23814fa50fb0045afd62661542d1d18e03d
Vannt <vannguyenthi5959@gmail.com>
2023-11-23 07:55:00 +0700

    update product

--- src/views/admin/Partner.js
+++ src/views/admin/Partner.js
@@ -7,26 +7,26 @@ import { FaUpload, FaTrash } from 'react-icons/fa';
 import "assets/css/anh.css";
 const Partner = () => {
 
-  const [uploadedFile, setUploadedFile] = useState(null);
+  const [selectedImages, setSelectedImages] = useState([]);
 
   const onDrop = useCallback((acceptedFiles) => {
-    const file = acceptedFiles[0];
-    setUploadedFile(file);
+    // Lưu trữ tất cả ảnh đã chọn
+    setSelectedImages((prevImages) => [...prevImages, ...acceptedFiles]);
   }, []);
+
+  const removeImage = (indexToRemove) => {
+    // Xóa ảnh theo chỉ số trong danh sách
+    setSelectedImages((prevImages) =>
+      prevImages.filter((_, index) => index !== indexToRemove)
+    );
+  };
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
     accept: 'image/*',
   });
-
-  const handleDelete = () => {
-    setUploadedFile(null);
-  };
-  const base64Data ='';
-  const dataUrl = `data:image/png;base64,${base64Data}`;
   return (
     <>
-      <Header />
       <Container className="mt--7" fluid>
         <Row>
           <div className="col">
@@ -35,35 +35,28 @@ const Partner = () => {
                 <h3 className="mb-0">Đối tác</h3>
               </CardHeader>
               <CardBody>
-           
-
-     
-                {/* <div>
-                  <img src={`data:image/png;base64,${}`} alt="Image" />
-                </div> */}
-        
-
-                <div className="upload-container">
-                  {uploadedFile ? (
-                    <div className="uploaded-image">
-                      <p>Ảnh đã tải lên:</p>
-                      <img src={URL.createObjectURL(uploadedFile)} alt={uploadedFile.name} />
-                      <button onClick={handleDelete} className="delete-button">
-                        <FaTrash />
-                      </button>
-                    </div>
-                  ) : (
-                    <>
-                      <div {...getRootProps()} className="dropzone">
-                        <input {...getInputProps()} />
-                      </div>
-                      <button className="upload-button">
-                        <FaUpload />
-                      </button>
-                    </>
-                  )}
-                </div>
-
+              <div className="image-uploader-container">
+      <div {...getRootProps()} className="dropzone">
+        <input {...getInputProps()} />
+        {selectedImages.length > 0 ? (
+          <div>
+            <p>Ảnh đã chọn:</p>
+            {selectedImages.map((image, index) => (
+              <div key={index} className="selected-image-container">
+                <img
+                  src={URL.createObjectURL(image)}
+                  alt={`Selected ${index}`}
+                  className="selected-image"
+                />
+                <button onClick={() => removeImage(index)}>Xóa</button>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>Kéo và thả ảnh hoặc click để chọn ảnh</p>
+        )}
+      </div>
+    </div>
               </CardBody>
             </Card>
           </div>
